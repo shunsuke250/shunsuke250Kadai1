acdfec77ea95150e2a1054bedf1d19cebc50406b
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-07-14 21:41:47 +0900

    [modify] textFieldを計算型プロパティで定義して可読性を向上

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -15,49 +15,42 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet private weak var textField5: UITextField!
     @IBOutlet private weak var sumCalculateButton: UIButton!
     @IBOutlet private weak var sumAnswerLabel: UILabel!
-
+    
+    private var textFields: [UITextField] {
+        [textField1, textField2, textField3, textField4, textField5]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sumCalculateButton.setTitle("合計値を計算する", for: .normal)
         sumAnswerLabel.text = "数値を入力して合計値を計算します"
-        textField1.delegate = self
-        textField2.delegate = self
-        textField3.delegate = self
-        textField4.delegate = self
-        textField5.delegate = self
-
-        let defaultValue = "0"
-        textField1.text = defaultValue
-        textField2.text = defaultValue
-        textField3.text = defaultValue
-        textField4.text = defaultValue
-        textField5.text = defaultValue
+        
+        textFields.forEach {
+            $0.delegate = self
+            $0.text = "0"
+        }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
+    
     @IBAction func calculateSumValue(_ sender: UIButton) {
-        var sum = 0
-        let textFields = [textField1, textField2, textField3, textField4, textField5]
-
-        for textField in textFields {
-            if let text = textField?.text, let num = Int(text) {
-                sum += num            }
-        }
+        let sum = textFields
+            .map { Int($0.text ?? "0") ?? 0 }
+            .reduce(0, +)
         sumAnswerLabel.text = "合計：\(String(sum))"
     }
-
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
         textField.text = ""
     }
-
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let text = textField.text else { return false }
         let newLength = text.count + string.count - range.length
         let maxLength = 10
-
+        
         return newLength <= maxLength
     }
 }
